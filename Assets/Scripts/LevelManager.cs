e391bd814f23b69918eb26a8a31b0f12e7340958
Ceren Avci <s.cerenavci@gmail.com>
2016-07-13 16:26:49 +0300

    Added Start and Win Scenes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void LoadLEvel (string name){
+	
+		Debug.Log("Level load requested for: " +name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+	
+		Debug.Log("I wanna quit !");
+		Application.Quit();
+	}
+}
