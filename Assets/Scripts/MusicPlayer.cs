b39d06160ec2d2a59a607d949ea5991a4991ce5a
Ceren Avci <s.cerenavci@gmail.com>
2016-07-28 14:33:44 +0300

    Added background image

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,9 +3,23 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+	static MusicPlayer instance = null;
+	
+	void Awake(){
+		Debug.Log ("Music player Awake " + GetInstanceID());
+		if (instance != null) {
+			Destroy(gameObject);
+			print("Duplicate music player self-destructing!");
+		}else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad(gameObject);
+		Debug.Log ("Music player Start " + GetInstanceID());
+
 	}
 	
 	// Update is called once per frame
