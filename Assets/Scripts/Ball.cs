266cc9bc7c82d6965655eee64ecbd3b7d38605be
Ceren Avci <s.cerenavci@gmail.com>
2016-07-29 21:00:31 +0300

    edited backgroud image and ball object positions

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	private Vector3 paddleToBallVector;
+	
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+
+	}
+	
+	void Update () {
+		this.transform.position = paddle.transform.position + paddleToBallVector;
+	}
+}
