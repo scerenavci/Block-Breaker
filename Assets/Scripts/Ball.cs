5704647fa5de265bfa37bfe435b0f57284e31d3b
Ceren Avci <s.cerenavci@gmail.com>
2016-07-29 21:39:05 +0300

    Created left,top and right colliders

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 
 	public Paddle paddle;
+	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
 	
 	void Start () {
@@ -12,6 +13,16 @@ public class Ball : MonoBehaviour {
 	}
 	
 	void Update () {
-		this.transform.position = paddle.transform.position + paddleToBallVector;
+		if (!hasStarted) {
+				this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			if (Input.GetMouseButtonDown(0)) {
+				print("mouse clicked, launch ball");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
+		}
 	}
 }
