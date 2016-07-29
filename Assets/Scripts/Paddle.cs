23ef5abe3d66a8a78ea561b03e6e15559d20ae44
Ceren Avci <s.cerenavci@gmail.com>
2016-07-29 20:35:28 +0300

    Added movement by mouse on the paddle

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks,0.5f,15.5f);
+		this.transform.position = paddlePos;
+	}
+}
