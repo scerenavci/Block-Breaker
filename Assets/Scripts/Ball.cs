8a995343dcc3f9c89e3ac26ad5d740cb6cb8c79b
Ceren Avci <s.cerenavci@gmail.com>
2016-07-31 15:33:17 +0300

    Created Level_03 scene and related objects

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
-	public Paddle paddle;
+	private Paddle paddle;
+
 	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
 	
 	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
 		paddleToBallVector = this.transform.position - paddle.transform.position;
-
 	}
 	
 	void Update () {
