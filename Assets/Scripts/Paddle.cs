87b1edd1c6b0247b1e79874932e886f54a52b503
Ceren Avci <s.cerenavci@gmail.com>
2016-08-03 23:33:32 +0300

    Fixed Paddle's z position issue

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -8,7 +8,7 @@ public class Paddle : MonoBehaviour {
 	}
 	
 	void Update () {
-		Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+		Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, -8f);
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
 		paddlePos.x = Mathf.Clamp(mousePosInBlocks,0.5f,15.5f);
 		this.transform.position = paddlePos;
