e30b8ab98cc71d5cba9f3572a6bad73760908d44
Ceren Avci <s.cerenavci@gmail.com>
2016-09-06 00:53:30 +0300

    Closed #5

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 
-	public void LoadLEvel (string name){
+	public void LoadLevel (string name){
 	
 		Debug.Log("Level load requested for: " +name);
 		Application.LoadLevel(name);
